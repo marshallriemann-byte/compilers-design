cfdea3d15a0f3cf1ba9a2f1a9bc3d6c315fd17d3
Hatim Muhammed Sulaiman Abd Al-Rahman <marshallriemann@gmail.com>
2025-09-03 21:15:32 +0200

    NFA class with constructor

---
+++ nfa.py
@@ -0,0 +1,21 @@
+# Nondeterministic finite automaton
+
+from collections import deque
+
+EMPTY_STRING = ''
+
+
+class NFA:
+    def __init__(
+        self,
+        states: set[str],
+        alphabet: set[str],
+        transition_function: dict[str, dict[str, set[str]]],
+        start_state: str,
+        accept_states: set[str]
+    ):
+        self.states = states
+        self.alphabet = alphabet
+        self.transition_function = transition_function
+        self.start_state = start_state
+        self.accept_states = accept_states
