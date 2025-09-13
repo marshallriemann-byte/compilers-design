7f5064a8ce995a1de8a76ec1aa240f6f0d09377d
Hatim Muhammed Sulaiman Abd Al-Rahman <marshallriemann@gmail.com>
2025-09-13 20:38:18 +0200

    That was a mistake

--- nfa.py
+++ nfa.py
@@ -159,7 +159,6 @@ class NFA:
         )
 
     def compute_minimized_DFA(self):
-        self = self.compute_equivalent_DFA().rename_states()
         partitions = set()
         X = frozenset(self.accept_states)
         if X:
